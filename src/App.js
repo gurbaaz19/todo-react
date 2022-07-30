6f1823cc6c54b228b22def66adc4e08e0340ecd7
Gurbaaz Singh Gill <gurbaaz19@gmail.com>
2022-07-31 04:03:39 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -26,15 +26,17 @@ function App() {
         console.log("Add", newTodo);
     }
 
-    const [todos, setTodos] = useState([{
-        sno: 1, title: "Learn React", desc: "Learn React and its ecosystem",
-    },
-
-        {
-            sno: 2, title: "Learn Angular", desc: "Learn Angular and its ecosystem",
-        }, {
-            sno: 3, title: "Learn Node", desc: "Learn Node and its ecosystem"
-        }]);
+    const [todos, setTodos] = useState([
+    //     {
+    //     sno: 1, title: "Learn React", desc: "Learn React and its ecosystem",
+    // },
+    //
+    //     {
+    //         sno: 2, title: "Learn Angular", desc: "Learn Angular and its ecosystem",
+    //     }, {
+    //         sno: 3, title: "Learn Node", desc: "Learn Node and its ecosystem"
+    //     }
+        ]);
 
     return (<>
         <Header title="To-Do List" searchBar={false}/>
--- src/MyComponents/Todos.js
+++ src/MyComponents/Todos.js
@@ -4,14 +4,23 @@ import {TodoItem} from "./TodoItem";
 export const Todos = (props) => {
     let containerStyle = {
         minHeight: "80vh",
-        margin:"60px auto",
+        margin: "60px auto",
     }
 
+    let titleStyle = {
+        display:"none",
+    }
+
+    if(props.todos.length > 0){
+        titleStyle.display = "block";
+    }
     return (<div className="container" style={containerStyle}>
-            <h3 className="text-center">To-Do List</h3>
-        {props.todos.length===0? "No To-Do Items" :
+        <h2 className="text-center py-3" style={titleStyle}>To-Do List</h2>
+        {props.todos.length === 0 ?
+            <h2 className="text-center py-5">No To-Do Items</h2>
+            :
             props.todos.map((todo) => {
-                return < TodoItem todo={todo} key={todo.sno} onDelete={props.onDelete} />
-            })}
-        </div>)
+            return < TodoItem todo={todo} key={todo.sno} onDelete={props.onDelete} />
+        })}
+    </div>)
 }
