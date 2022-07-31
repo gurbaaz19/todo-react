e215a9579da32760efb7396911e62358c55b12ca
Gurbaaz Singh Gill <gurbaaz19@gmail.com>
2022-07-31 05:56:29 +0530

    Finished

--- src/MyComponents/Header.js
+++ src/MyComponents/Header.js
@@ -13,6 +13,7 @@ export const Header = (props) => {
                         aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                     <li className="nav-item">
                         <Link className="nav-link active" aria-current="page" to="/">Home</Link>
@@ -26,6 +27,7 @@ export const Header = (props) => {
                     aria-label="Search"/>
                     <button className="btn btn-outline-success" type="submit">Search</button>
                     </form>:null}
+                </div>
             </div>
         </nav>
     );
